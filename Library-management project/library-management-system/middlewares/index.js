4e5d0c0b7bf12ccc6a7e207bdc49ba689a02b704
devbhati582 <devbhati582@gmail.com>
2024-09-25 22:09:43 +0530

    Delete Library-management project directory

--- Library-management project/library-management-system/middlewares/index.js
+++
@@ -1,9 +0,0 @@
-const authMiddleware = require("./auth.middleware");
-const authrorizationMiddleware  = require("./authorization.middlware");
-const loggedInMiddleware = require("./loggedIn.middleware");
-
-module.exports = {
-    authMiddleware,
-    authrorizationMiddleware,
-    loggedInMiddleware
-}
